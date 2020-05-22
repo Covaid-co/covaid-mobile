92802cea8a1ba3214ebbe60f721abb242641a494
Angela Luo <ael49021@gmail.com>
2020-05-22 15:27:54 -0500

    frontend for login screen

--- screens/LoginScreen/LoginScreen.js
+++ screens/LoginScreen/LoginScreen.js
@@ -1,16 +1,59 @@
-import React from "react";
-import { View, Text, Button, Image, StatusBar, Alert } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  StatusBar,
+  Alert,
+} from "react-native";
 import { styles, buttons, texts } from "./LoginScreenStyles";
 
 export default function LoginScreen() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  function handleLogin() {
+    console.log(username);
+    console.log(password);
+  }
+
+  function handlePasswordReset() {
+    console.log("send email");
+  }
   return (
-    <View style={styles.container}>
-      <View>
-        <Image source={require("../../assets/images/robot-dev.png")} />
-        <Text style={texts.header}>Mutual aid for COVID-19</Text>
-      </View>
-      <View>
-        <Button style={buttons.primary} title="Login" />
+    <View>
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/images/C-LOGO.png")}
+        />
+        <Text style={texts.header}>Volunteer App for COVID-19</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#7F7F7F"
+          onChangeText={(text) => setUsername(text)}
+          defaultValue={username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="#7F7F7F"
+          onChangeText={(text) => setPassword(text)}
+          defaultValue={password}
+        />
+        <TouchableOpacity onPress={handlePasswordReset}>
+          <Text style={texts.button_label_blue}>Forgot your Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={buttons.login} onPress={handleLogin}>
+          <Text style={texts.button_label}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={buttons.signup}>
+          <Text style={texts.button_label_blue}>SIGN UP</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
