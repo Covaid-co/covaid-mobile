9382872e1ccc290d06f791b25802bb57e004db9d
Shresta Bangaru <shrestab19@gmail.com>
2020-05-25 14:46:35 -0500

    In progress requests screen

--- screens/RequestsScreen/RequestsScreen.js
+++ screens/RequestsScreen/RequestsScreen.js
@@ -11,10 +11,45 @@ import { styles, buttons, texts } from "./RequestsScreenStyles";
 import { homeURL } from "../../constants";
 import { generateURL, validateEmail } from "../../Helpers";
 import fetch_a from '../../util/fetch_auth'
+import { createIconSetFromFontello } from "@expo/vector-icons";
 
-export default function LoginScreen() {
+export default function RequestsScreen() {
   const [userID, setUserID] = useState("5ec1a0b411b7cc136655d8d0");
   const [user, setUser] = useState(); 
+  const [login]
+
+  const handleLogin = () => {
+    fetch(homeURL + "/api/users/login/", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    })
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((data) => {
+            setUserID(data["user"]._id);
+            fetch_user_obj(data["user"]._id);
+          });
+        } else {
+          if (response.status === 403) {
+            Alert.alert(
+              "Check your email for a verification link prior to logging in.",
+              ""[{ text: "OK" }],
+              {
+                cancelable: false,
+              }
+            );
+          } else if (response.status === 401) {
+            Alert.alert("Incorrect username or password", ""[{ text: "OK" }], {
+              cancelable: false,
+            });
+          }
+        }
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  }
 
   const getRequests = () => {
     let params = {'status': 1};
@@ -22,14 +57,15 @@ export default function LoginScreen() {
     fetch_a('token', url, {
             method: 'get',
     }).then((response) => {
-        alert(JSON.stringify(response))
+        console.log(JSON.stringify(response))
         if (response.ok) {
             response.json().then((data) => {
                 console.log("what")
                 alert(JSON.stringify(data)); 
             });
         } else {
-            alert("Error obtaining requests");
+          console.log("HELO")
+          alert("Error obtaining requests");
         }
     }).catch((e) => {
         alert(e);
