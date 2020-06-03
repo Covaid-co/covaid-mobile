9ee3696819355af66a432b561f63ebdae2e9519f
Angela Luo <ael49021@gmail.com>
2020-06-03 00:23:20 -0500

    added in zipcode

--- screens/EditProfileScreen/EditProfileScreen.js
+++ screens/EditProfileScreen/EditProfileScreen.js
@@ -15,6 +15,8 @@ import { generateURL, validateEmail, extractTrueObj } from "../../Helpers";
 import CheckForm from "../../components/CheckForm/CheckForm";
 import Details from "../../components/Details/Details";
 import Colors from "../../public/Colors";
+import Geocode from "react-geocode";
+import fetch_a from '../../util/fetch_auth';
 /**
  * Update location when zipcode changes
  *
@@ -96,28 +98,47 @@ export default function LoginScreen({ route, navigation }) {
   };
 
   const setConstants = (data) => {
-    setFirstName(data.first_name);
-    setLastName(data.last_name);
-    setEmail(data.email);
-    setPhone(data.phone);
-    //zip code using a location function
-    //maybe on location change, it will automatically rerender the page and switch the categories
-    setDetails(data.offer.details);
-    setCurrentUserObject(data.languages, languages, setLanguageChecked);
-    setCurrentUserObject(data.offer.timesAvailable, timeNames, setTimes);
-    async function getResources() {
-      if (!data.association) {
-        setCurrentUserObject(data.offer.tasks, defaultResources, setResources);
-        return;
-      }
-      let params = { associationID: data.association };
-      var url = generateURL("/api/association/get_assoc/?", params);
-      const response = await fetch(url);
-      response.json().then((res) => {
-        setCurrentUserObject(pdata.offer.tasks, res.resources, setResources);
+    let params = {};
+    var url = generateURL(homeURL + '/api/apikey/google', params);
+    fetch(url).then((response) => {
+      if (response.ok) {
+      response.json().then(key => {
+          Geocode.setApiKey(key['google']);
+          setIsLoaded(true)
+          setFirstName(data.first_name);
+          setLastName(data.last_name);
+          setEmail(data.email);
+          setPhone(data.phone);
+          console.log(data)
+          setLatLong(data.latlong);
+          getZip(data.latlong);
+          setAssociation(data.association);
+          setAssociationName(data.association_name);
+          //zip code using a location function
+          //maybe on location change, it will automatically rerender the page and switch the categories
+          setDetails(data.offer.details);
+          setCurrentUserObject(data.languages, languages, setLanguageChecked);
+          setCurrentUserObject(data.offer.timesAvailable, timeNames, setTimes);
+          async function getResources() {
+            if (!data.association) {
+              setCurrentUserObject(data.offer.tasks, defaultResources, setResources);
+              return;
+            }
+            let params = { associationID: data.association };
+            var url = generateURL("/api/association/get_assoc/?", params);
+            const response = await fetch(url);
+            response.json().then((res) => {
+              setCurrentUserObject(pdata.offer.tasks, res.resources, setResources);
+            });
+          }
+          getResources();
       });
+    } else {
+      console.log("Error");
     }
-    getResources();
+  }).catch((e) => {
+    alert(e);
+  });
   };
   const setCurrentUserObject = (userList, fullList, setFunction) => {
     for (var i = 0; i < fullList.length; i++) {
@@ -129,6 +150,135 @@ export default function LoginScreen({ route, navigation }) {
       }));
     }
   };
+
+  const updateLocation = async e => {
+    if (zip.length !== 5 || !(/^\d+$/.test(zip))) {
+        setToastMessage('Invalid zip');
+        setShowToast(true);
+        return;
+    }
+    if (initialZip !== zip) {
+        await handleChangedZip();
+    } else {
+        setNeighborhoods(user.offer.neighborhoods)
+        setAssociation(user.association)
+        setAssociationName(user.association_name)
+        setLatLong(user.latlong)
+        setShowChangeAssocModal(false)
+        setCurrentUserObject(user.offer.tasks, defaultResources, setResources);
+    }
+}
+
+function getZip(location) {
+  var latlng = {lat: parseFloat(location[1]), lng: parseFloat(location[0])};
+  var latitude = latlng.lat;
+  var longitude = latlng.lng;
+
+  Geocode.fromLatLng(latitude, longitude).then(
+      response => {
+          if (response.status === 'OK') {
+              for (var i = 0; i < response.results.length; i++) {
+                  for (var j = 0; j < response.results[i].address_components.length; j++) {
+                      if (response.results[i].address_components[j].types.indexOf('postal_code') > -1) {
+                          setInitialZip(response.results[i].address_components[j].long_name)
+                          setZip(response.results[i].address_components[j].long_name);
+                          break;
+                      }
+                  }
+              }
+          }
+      }
+  );
+}
+
+const handleNoAssociations = () => {
+  setDefaultResources(defaultTaskList)
+  setCurrentUserObject([], defaultTaskList, setResources);
+  var temp_resources = {}
+  for (var i = 0; i < defaultTaskList.length; i++) {
+      temp_resources[defaultTaskList[i]] = false
+  }
+  setAssociation('')
+  setAssociationName('')
+  setResources(temp_resources)
+  setShowChangeAssocModal(true)
+}
+
+const handleNewAssociation = (association) => {
+  setDefaultResources(association.resources)
+  setCurrentUserObject([], association.resources, setResources);
+  var temp_resources = {}
+  for (var i = 0; i < association.resources.length; i++) {
+      temp_resources[association.resources[i]] = false
+  }
+  setResources(temp_resources)
+  setAssociation(association._id)
+  setAssociationName(association.name)
+  setShowChangeAssocModal(true)
+}
+
+async function getLatLng(zip) {
+  try {
+      if (zip.length !== 5 || !(/^\d+$/.test(zip))) {
+          throw Error('Invalid zip');
+      }
+      var response = await Geocode.fromAddress(zip);
+      var new_neighborhoods = []
+      var foundState = [];
+      for (var i = 0; i < Math.min(5, response.results.length); i++) {
+          const results = response.results[i]['address_components'];
+          for (var j = 0; j < results.length; j++) {
+            const types = results[j].types;
+            if (types.includes('neighborhood') || types.includes('locality')) {
+              const currNeighborhoodName = results[j]['long_name'];
+              if (new_neighborhoods.includes(currNeighborhoodName) === false) {
+                new_neighborhoods.push(currNeighborhoodName);
+              }
+            }
+            for (var k = 0; k < types.length; k++) {
+              const type = types[k];
+              if (foundState.length === 0 && type === "administrative_area_level_1") {
+                foundState = [results[j]['long_name'], results[j]['short_name']];
+              }
+            }
+          }
+      }
+      const { lat, lng } = response.results[0].geometry.location;
+      setLatLong([lng, lat])
+      let params = {'latitude': lat, 'longitude': lng}
+      const url = generateURL("/api/association/get_assoc/lat_long?", params);
+      const response_assoc = await fetch(url);
+      const data = await response_assoc.json();
+      setZipUpdated(true)
+      if (user.association !== '' && data.length === 0) {
+          setNeighborhoods(new_neighborhoods)
+          setFoundState(foundState)
+          handleNoAssociations()
+      } else if (data.length > 0 && user.association !== data[0]['_id']) {
+          setNeighborhoods(new_neighborhoods)
+          setFoundState(foundState)
+          handleNewAssociation(data[0])
+      } else {
+          setNeighborhoods(new_neighborhoods)
+          setFoundState(foundState)
+          setAssociation(user.association)
+          setAssociationName(user.association_name)
+          setShowChangeAssocModal(false)
+          setCurrentUserObject(user.offer.tasks, defaultResources, setResources);
+      }
+      return true
+  } catch (err) {
+      alert('Invalid zip')
+  }
+}
+
+const handleChangedZip = () => {
+  if (getLatLng(zip)) {
+      return true;
+  } else {
+      return false;
+  }
+}
 
   function form(header, change, value) {
     return (
