87834d8cd752d613218cf68142231bf5df151837
Ashwin Saxena <ashwin.saxena1@gmail.com>
2020-07-30 20:25:44 -0500

    Profile pic (#47)
    * In progress requests screen
    
    * Requests Screen frontend (#19)
    
    * Setting up formatting, fonts, and pages
    
    * frontend for login screen
    
    * Login works, gets user ID
    
    * Getting user object from ID
    
    * form verification and password protection
    
    * made alerts prettier
    
    * In progress using token
    
    * Fetching requests works
    
    * In progress display requests
    
    * Fixing frontend display requests
    
    * Fixing css frontend
    
    * hard coded in some profile stuff
    
    * Login to Request transition
    
    * Modal pop up if request clicked
    
    * Separating request types
    
    * Separate modal for each request type
    
    * minor css change
    
    * Async storage, has bugs
    
    * added offer portion to the profile page
    
    * passing in parameters to the Edit your profile screen
    
    * passing in userid for user object
    
    * Getting all requests in useEffect()
    
    * Async storage is working
    
    * less hardcoding
    
    * Navigation between login and request screen
    
    * Request pages frontend
    
    * Request frontend
    
    * basic typing UI
    
    * added in languages - WIP buttons dont press
    
    * fixed checkForm functionality
    
    * car toggle
    
    * added in task selector
    
    * finished frontend except for location
    
    * latest code
    
    * Fixing merge issue
    
    * Fixing merge
    
    * Navigation using routes
    
    * frontend fixes
    
    * Frontend fixes
    
    * Adding number of request badges to frontend requests page
    
    * pushing package.json
    
    * forgot your password, ui changes
    
    * Removing my machine specific changes
    
    Co-authored-by: Angela Luo <ael49021@gmail.com>
    Co-authored-by: Shresta Bangaru <bangaru2@illinois.edu>
    
    * Selects img from camera roll and displays img
    
    * working profile pic
    
    * cleaning up code
    
    * update yarn.lock
    
    Co-authored-by: Shresta Bangaru <shrestab19@gmail.com>
    Co-authored-by: Shresta Bangaru <51917387+shresta4@users.noreply.github.com>
    Co-authored-by: Angela Luo <ael49021@gmail.com>
    Co-authored-by: Shresta Bangaru <bangaru2@illinois.edu>
    Co-authored-by: debanikpurkayastha <debanik1997@gmail.com>

--- components/ProfileHeader/ProfileHeader.js
+++ components/ProfileHeader/ProfileHeader.js
@@ -1,30 +1,15 @@
 import React, { useEffect, useState } from "react";
-//import "bootstrap/dist/css/bootstrap.min.css";
-// import Button from "react-bootstrap/Button";
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import Image from "react-bootstrap/Image";
-// import Modal from "react-bootstrap/Modal";
-import { generateURL } from "../../Helpers";
-//import ImageUploader from "react-images-upload";
-//import ImagePicker from 'react-native-image-picker'
 import {
-  Text,
   TouchableOpacity,
   View,
-  ScrollView,
-  Switch,
-  ActivityIndicator,
   AsyncStorage,
   Image,
-  Modal,
-  TextInput,
   Alert,
-  Keyboard,
 } from "react-native";
+import * as Permissions from "expo-permissions";
 import fetch_a from "../../util/fetch_auth";
-import { homeURL } from "../../constants";
+import { homeURL, storage_keys } from "../../constants";
+import ProfilePicturePicker from "./ProfilePicturePicker.js";
 
 export default function ProfileHeader(props) {
   const [association, setAssociation] = useState("");
@@ -32,31 +17,11 @@ export default function ProfileHeader(props) {
     "https://www.csfences.com/wp-content/uploads/2016/08/profile-placeholder.jpg"
   );
   const [uploadingImage, setUploadingImage] = useState({});
-  const [isUploaded, setIsUploaded] = useState(false);
-  const [showUploader, setShowUploader] = useState(false);
+  //const [isUploaded, setIsUploaded] = useState(false);
+  //const [showUploader, setShowUploader] = useState(false);
   const [imageUrl, setImageUrl] = useState("/api/image/" + props.user._id);
-
-  const onDrop = (pictureFiles, pictureDataURLs) => {
-    setUploadingImage(pictureFiles[0]);
-    setIsUploaded(true);
-  };
-
-  const upload = (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("file", uploadingImage);
-
-    fetch_a("token", "/api/image", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => {
-        response.json().then((data) => {
-          window.location.reload(false);
-        });
-      })
-      .catch((err) => alert("Error: " + err));
-  };
+  const [showImagePicker, setShowImagePicker] = useState(false);
+  const [openCameraRoll, setOpenCameraRoll] = useState();
 
   const fetchProfilePic = (id) => {
     fetch(homeURL + "/api/image/" + id).then((response) => {
@@ -70,112 +35,98 @@ export default function ProfileHeader(props) {
     });
   };
 
+  function uploadProfilePic(uri) {
+    let formData = new FormData();
+    formData.append("file", {
+      uri: uri.replace("file://", ""),
+      name: "file",
+      type: "image/jpg",
+    });
+
+    AsyncStorage.getItem(storage_keys.SAVE_TOKEN_KEY).then((token) => {
+      fetch_a(token, "token", homeURL + "/api/image", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          response.json().then((data) => {
+            setImageUrl(uri);
+          });
+        })
+        .catch((err) => alert("Error: " + err));
+    });
+  }
+
+  function handleUpdatePicture() {
+    Alert.alert(
+      "Would you like to upload an existing photo from camera roll?",
+      "",
+      [
+        /*{  
+          text: 'Take Photo',  
+          onPress: () => {
+            setOpenCameraRoll(false); 
+            setShowImagePicker(true);
+          },   
+        },  */
+        {
+          text: "Upload",
+          onPress: () => {
+            setOpenCameraRoll(true);
+            setShowImagePicker(true);
+          },
+        },
+        {
+          text: "Cancel",
+          onPress: () => {
+            setOpenCameraRoll(false);
+            setShowImagePicker(false);
+          },
+        },
+      ]
+    );
+  }
+
   useEffect(() => {
+    setShowImagePicker(false);
     fetchProfilePic(props.user._id);
   }, [props.user]);
 
   return (
     <>
-      {/* <div style={{ marginLeft: 35, maxWidth: 2000 }}>
-        <Row>
-          <Col> */}
       <View>
-        <Image
-          source={{ uri: imageUrl }}
-          //source={require("../../assets/images/C-LOGO.png")}
-          id="profile-pic"
-          style={{
-            height: 115,
-            width: 115,
-            borderRadius: 107,
-            resizeMode: "stretch",
-            margin: 5,
-          }}
-          // style={{
-          //   marginRight: 30,
-          //   boxShadow:
-          //     "0 2px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.1)",
-          //   cursor: "pointer",
-          // }}
-          onClick={() => setShowUploader(true)}
-        />
-      </View>
-
-      {/* <Row>
-              <h1
-                id="home-heading"
-                style={{
-                  marginTop: 20,
-                  marginBottom: 0,
-                  display: "inline-block",
-                  fontSize: 28,
-                  color: "#4F4F4F",
-                }}
-              >
-                {props.user.first_name} {props.user.last_name}
-              </h1>
-            </Row> */}
-      {/* <Row>
-              <p
-                id="regular-text"
-                style={{
-                  marginLeft: 1,
-                  marginTop: 4,
-                  fontSize: 16,
-                  marginBottom: 10,
-                }}
-              >
-                {association && association.length > 0
-                  ? association
-                  : "Covaid Volunteer"}{" "}
-              </p>
-            </Row> */}
-      {/* <Row>
-              <Button
-                id="medium-button"
-                style={{
-                  paddingLeft: 34,
-                  paddingRight: 34,
-                  paddingTop: 10,
-                  paddingBottom: 10,
-                }}
-                onClick={() => {
-                  props.setShowAccountModal(true);
-                }}
-              >
-                Edit Profile
-              </Button>{" "}
-            </Row> */}
-      {/* </Col>
-        </Row>
-      </div> */}
-      {/* <Modal
-        show={showUploader}
-        onHide={() => {
-          setShowUploader(false);
-          setIsUploaded(false);
-          setUploadingImage({});
-        }}
-        style={{ marginTop: 10, paddingBottom: 50 }}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Upload a new profile picture</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <ImageUploader
-            withIcon={true}
-            buttonText="Choose an image"
-            onChange={onDrop}
-            imgExtension={[".jpg", ".png"]}
-            maxFileSize={5242880}
-            singleImage={true}
-            withPreview={true}
+        {showImagePicker && (
+          <ProfilePicturePicker
+            setImageUrl={setImageUrl}
+            setShowImagePicker={setShowImagePicker}
+            uploadProfilePic={uploadProfilePic}
+            openCameraRoll={openCameraRoll}
+            setOpenCameraRoll={setOpenCameraRoll}
           />
-          <Button disabled={!isUploaded} id="large-button" onClick={upload}>
-            Upload
-          </Button>
-        </Modal.Body>
-      </Modal> */}
+        )}
+
+        <TouchableOpacity
+          onPress={() => {
+            console.log("bring up edit options!!!");
+            handleUpdatePicture();
+          }}
+        >
+          <Image
+            source={{ uri: imageUrl }}
+            id="profile-pic"
+            style={{
+              height: 115,
+              width: 115,
+              borderRadius: 107,
+              resizeMode: "stretch",
+              margin: 5,
+            }}
+            onClick={() => {
+              handleUpdatePicture();
+            }}
+          />
+        </TouchableOpacity>
+      </View>
     </>
   );
 }
---
+++ components/ProfileHeader/ProfilePicturePicker.js
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import DeprecatedViewPropTypes from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedViewPropTypes';
+
+export default class ProfilePicturePicker extends React.Component {
+  state = {
+    image: null,
+    select: true, 
+  };
+
+  render() {
+    let { image } = this.state;
+    if (this.props.openCameraRoll) {
+      console.log("Picking image")
+      this._pickImage(); 
+    } /*else {
+      console.log("Taking picture")
+      this._takeImage(); 
+    }*/
+    return (
+      <></>
+    );
+  }
+
+  componentDidMount() {
+    this.getPermissionAsync();
+  }
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios /*&& this.props.openCameraRoll*/) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+        this.props.setShowImagePicker(false); 
+        this.props.setOpenCameraRoll(false);
+      }
+    } /*else if (Constants.platform.ios && !this.props.openCameraRoll) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera permissions to make this work!');
+        this.props.setShowImagePicker(false); 
+      }
+    }*/
+  };
+
+  _pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.props.setShowImagePicker(false); 
+        this.props.uploadProfilePic(result.uri); 
+      }
+      this.props.setShowImagePicker(false); 
+    } catch (E) {
+      console.log(E);
+    }
+  };
+
+  /*_takeImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+        this.props.setImageUrl(result.uri); 
+        this.props.uploadProfilePic(results.uri); 
+      }
+      //this.setState({ select: false });
+      this.props.setShowImagePicker(false); 
+
+      console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
+  };*/
+}
